ac90aec8a66b3a327e7985718aa7a81f31eedb1e
Mariana Tapia <mglopez2113@gmail.com>
2022-04-05 21:03:10 -0700

    added js

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
+var startQuiz
+var timeInterval
+var question1
+var dataAtrributes
+var nesting
+var question2
+var cssQUes
+var question3
+var jsQuest
+var startBtn
+
+secondsLeft = 0
+
+
+function startQuiz() {
+
+}
+
+
 document.querySelector("#start-quiz").addEventListener("click", function() {
 
     window.location.href = "./index.html";
 });
+
+
+
+var timeInterval = setInterval(function() {
+    secondsLeft--;
+    timeInterval.textContent = "Time left :" + secondsLeft;
+    if(secondsLeft === 0) {
+        clearInterval(timeInterval);
+    }
+}, 1000);
+
+
+alert(timeInterval)
+
+
+
+
 
